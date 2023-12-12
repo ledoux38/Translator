4fc7e24327ef6147933b81b902fd073991458afc
ledoux <ledoux.florian30@gmail.com>
2023-12-12 14:11:44 +0100

    Develop (#1)
    fix program and add unittest

---
+++ unittest/unittest.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json.Linq;
+using translator;
+using static NUnit.Framework.Assert;
+
+namespace unittest;
+
+[TestFixture]
+public class TranslationUpdaterTests
+{
+    [Test]
+    public void LoadJson_ValidFile_ShouldReturnJObject()
+    {
+        var jsonContent = TranslationUpdater.LoadJson("testData/fr.json");
+        IsNotNull(jsonContent);
+        That(jsonContent["greeting"]?.ToString(), Is.EqualTo("Bonjour"));
+    }
+    
+    // [Test]
+    // public void SaveJson_ShouldWriteToFile()
+    // {
+    //     // Création de l'objet JSON fictif
+    //     var targetJson = new JObject
+    //     {
+    //         ["exampleKey"] = "exampleValue"
+    //     };
+    //
+    //     // Appel de la méthode SaveJson
+    //     TranslationUpdater.SaveJson( "en.json", targetJson);
+    //
+    //     // Vérifier que le fichier a été créé
+    //     Assert.IsTrue(File.Exists( "en.json"));
+    //
+    //     // Vérifier le contenu du fichier
+    //     var savedJson = JObject.Parse(File.ReadAllText("en.json"));
+    //     Assert.AreEqual("exampleValue", savedJson["exampleKey"].ToString());
+    // }
+
+}
